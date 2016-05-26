2fa9fb9cc8ff0dedc845e1d57a645573b5a8d60c
Marko Nikic <darkblue.azurite@gmail.com>
2016-05-26 15:13:01 +0200

    Add files via upload

--- min_version/DungeonGenerator.js
+++ min_version/DungeonGenerator.js
@@ -63,6 +63,8 @@ var Dungeon = {
 		);
 	},
 	Map: function(rows, cols, lists, entityIds) {
+		this.height = rows;
+		this.width = cols;
 		this.floor = entityIds.floor;
 		this.wall = entityIds.wall;
 		this.enemy = entityIds.enemy;
@@ -165,20 +167,13 @@ var Dungeon = {
 		}
 		return true;
 	},
-	createEntities: function(list, room) {
+	createEntities: function(list, room, type) {
 		var e_max = Dungeon.Helper.random(0,1);
 		for(var i = 0; i < e_max; i++) {
 			do {
-				var enemy = new Dungeon.Entity(room, "enemy");
-			} while(!Dungeon.itemPositionIsUnique(list, enemy));
-			list.push(enemy);
-		}
-		var h_max = Dungeon.Helper.random(0,1);
-		for(var j = 0; j < h_max; j++) {
-			do {
-				var health = new Dungeon.Entity(room, "health");
-			} while(!Dungeon.itemPositionIsUnique(list, health));
-			list.push(health);
+				var item = new Dungeon.Entity(room, type);
+			} while(!Dungeon.itemPositionIsUnique(list, item));
+			list.push(item);
 		}
 		return list;
 	},
@@ -218,9 +213,15 @@ var Dungeon = {
 			entities.push(weapon);
 		}
 		
-		if(args.entities.enemy !== undefined && args.entities.health !== undefined) {
+		if(args.entities.enemy !== undefined) {
 			for(var ei = 0; ei < rooms.length; ei++) {
-				Dungeon.createEntities(entities, rooms[ei]);
+				Dungeon.createEntities(entities, rooms[ei], "enemy");
+			}
+		}
+
+		if(args.entities.health !== undefined) {
+			for(var hi = 0; hi < rooms.length; hi++) {
+				Dungeon.createEntities(entities, rooms[hi], "health")
 			}
 		}
 
